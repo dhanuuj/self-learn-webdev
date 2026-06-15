c43c87e854d5aacc9133f096f35e66fe33336758
Dhanuja Senarathna <kvdbuddhimas6711@gmail.com>
2026-06-16 00:57:27 +0530

    backend fule structure setup.2

---
+++ React/Movie App - Full Stack - JSMastery/crash-course/src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Search = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className='search'>
+        <div>
+
+            <img src='search.svg' alt='search'></img>
+
+            <input
+                type='text'
+                placeholder='Search through thousands of movies...'
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}>
+            </input>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Search
